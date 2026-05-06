44f9e962ea126043bf0ea7faf5592b6000f6d420
AVayron <alexis.vayrondelamoureyre@epfl.ch>
2026-05-06 14:12:49 +0200

    Update_conversion_program

--- Conversion_xyz_From_SMILES.py
+++ Conversion_xyz_From_SMILES.py
@@ -1,7 +1,12 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-def Mol_From_SMILES(smiles):
+def Mol_From_SMILES(smiles): #Obtention of the molecules
     mol=Chem.MolFromSmiles(smiles)
-    return(mol)
-
+    if mol == None:
+        raise ValueError("Invalid SMILES")#Rejection of wrong SMILES
+    else:
+        return mol
+    
+def Most_Stable_Conformer(mol):
+    
