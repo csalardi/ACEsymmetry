879b0170d47963aa6f092e4c3b1cbfdcfb794ffe
AVayron <alexis.vayrondelamoureyre@epfl.ch>
2026-05-10 15:00:57 +0200

    Correction of annotation, activation of the export of SDF file

--- Conversion_xyz_From_SMILES.py
+++ Conversion_xyz_From_SMILES.py
@@ -1,7 +1,6 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.rdchem import Mol
-from rdkit.Chem.AllChem import ForceField
 
 def Mol_From_SMILES(smiles:str):#treatment of SMILES input
     mol: Mol = Chem.MolFromSmiles(smiles, sanitize=True)
@@ -15,15 +14,15 @@ def Conformer_Selection(mol:Mol, num_confs=20):#Generation of conformers and sel
     conf_ids:list[int] = AllChem.EmbedMultipleConfs(mol, numConfs=num_confs)
     if not conf_ids:
         raise ValueError("No conformers generated")
-    energies:dict = {}
+    energies:dict[int, float] = {}
     for conf_id in conf_ids:
-        ff:ForceField = AllChem.UFFGetMoleculeForceField(mol, confId=conf_id)
+        ff = AllChem.UFFGetMoleculeForceField(mol, confId=conf_id)
         ff.Minimize()
         energies[conf_id] = ff.CalcEnergy()
     most_stable_conformer:int = min(energies, key=energies.get)
     mol_block:str=Chem.MolToMolBlock(mol,confId=most_stable_conformer)
-    #with open(filename, "w") as file:
-        #file.write(mol_block)
+    with open(filename, "w") as file:
+        file.write(mol_block)
     mol:Mol=Chem.MolFromMolBlock(mol_block, removeHs=False)
     return mol
   
@@ -36,7 +35,7 @@ def Overall_conversion(Smiles:str, filename:str, num_confs:int):
         file.write(xyz)
     return Chem.MolToXYZBlock(mol)
 
-print (Overall_conversion("C-C-O", "ethanol.SDF", 50))
+
 
 
 
