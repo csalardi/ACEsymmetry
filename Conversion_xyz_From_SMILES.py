938ffdf93224fe6ed6953cc507e2d8d59154d3d4
AVayron <alexis.vayrondelamoureyre@epfl.ch>
2026-05-10 12:02:48 +0200

    Code development

--- Conversion_xyz_From_SMILES.py
+++ Conversion_xyz_From_SMILES.py
@@ -1,12 +1,22 @@
 from rdkit import Chem#Dependancies
 from rdkit.Chem import AllChem
+import numpy as np
 
 def Mol_From_SMILES(smiles): #Obtention of the molecules
     mol=Chem.MolFromSmiles(smiles)
     if mol == None:
         raise ValueError("Invalid SMILES")#Rejection of wrong SMILES
     else:
+        mol=Chem.AddHs(mol)
         return mol
+
+def Conformer_Selection(mol):#Generation of conformers and selection of the most stable one
+    energies:dict={}
+    for conformer in mol.GetConformers():
+        ff = AllChem.UFFGetMoleculeForceField(mol, confId=conformer.GetId())
+        ff.Minimize()
+        energies{conformer}=ff
     
-def Most_Stable_Conformer(mol):
+
+
 
