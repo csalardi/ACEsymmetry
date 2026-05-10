b4c1744ebcb67ac2816c9242d07100aec057b044
AVayron <alexis.vayrondelamoureyre@epfl.ch>
2026-05-10 20:25:30 +0200

    simplification second function

--- Conversion_xyz_From_SMILES.py
+++ Conversion_xyz_From_SMILES.py
@@ -23,19 +23,18 @@ def Conformer_Selection(mol:Mol, num_confs, filename_1:str):#Generation of confo
     mol_block:str=Chem.MolToMolBlock(mol,confId=most_stable_conformer)
     with open(filename_1, "w") as file:
         file.write(mol_block)
-    mol:Mol=Chem.MolFromMolBlock(mol_block, removeHs=False)
-    return mol
+    return most_stable_conformer
   
 
 def Overall_conversion(Smiles:str,filename_1:str, filename_2:str, num_confs:int):
     mol:Mol=Mol_From_SMILES(Smiles)
-    mol:Mol=Conformer_Selection(mol, num_confs, filename_1)
-    xyz:str = Chem.MolToXYZBlock(mol, confId=0)
+    most_stable_conf:int=Conformer_Selection(mol, num_confs, filename_1)
+    xyz:str = Chem.MolToXYZBlock(mol, confId=most_stable_conf)
     with open(filename_2, "w") as file:
         file.write(xyz)
-    return Chem.MolToXYZBlock(mol)
+    return Chem.MolToXYZBlock(mol, confId=most_stable_conf)
 
-print (Overall_conversion("C-C-O","ethanol.SDF" ,"ethanol.xyz", 1000))
+print (Overall_conversion("C-C-O","ethanol.SDF" ,"ethanol.xyz", 2000))
 print (Overall_conversion("C(C)(C)(C)(C)(C)","pentavalent.SDF","pentavalent.xyz", 1000))
 
 
