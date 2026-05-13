ef1021a7b84f098327f7f1e6a2d7332a089ee97b
csalardi <cyrielle.salardi-brahy@epfl.ch>
2026-05-13 11:41:44 +0200

    Implementation of tests for the error handling

--- tests.py
+++ tests.py
@@ -4,3 +4,5 @@ print(f"Point group C1 contains the following symmetry elements: {get_symmetry_s
 print(f"Point group Ih contains the following symmetry elements: {get_symmetry_set('Ih')}")
 print(f"Point group Cinfv contains the following symmetry elements: {get_symmetry_set('Cinfv')}")
 print(f"Point group D2h contains the following symmetry elements: {get_symmetry_set('D2h')}")
+#print(f"Point group 4 contains the following symmetry elements: {get_symmetry_set(4)}") # Raise a type error
+#print(f"Point group Dinf contains the following symmetry elements: {get_symmetry_set('Dinf')}") # Raise a value error
