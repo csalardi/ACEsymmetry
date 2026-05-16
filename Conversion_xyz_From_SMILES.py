93cdd76e612a75413f397e2c76ef79012367d824
AVayron <alexis.vayrondelamoureyre@epfl.ch>
2026-05-16 22:52:20 +0200

    Major update in the generation of xyz file from the smiles. The update is based on the work performed previously on the xyz from the name

--- Conversion_xyz_From_SMILES.py
+++ Conversion_xyz_From_SMILES.py
@@ -2,22 +2,6 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.rdchem import Mol
 from typing import Union
-import pubchempy as pcp
-
-def smiles_obtention (molecule_name: str)-> str:
-    '''
-    Gives the smiles of a molecule from one of its 
-    common non systematic names.
-
-    Args:
-        molecule_name (str): name of the studied molecule
-    
-    Returns:
-        str: smiles of the molecule
-    '''
-    for compound in pcp.get_compounds (molecule_name, "name"):
-        return compound.smiles
-
 
 def mol_from_SMILES(smiles: str)-> Union[Mol, None]:
     '''
@@ -35,10 +19,47 @@ def mol_from_SMILES(smiles: str)-> Union[Mol, None]:
     if mol == None:
         return(None)
     else:
-        mol = Chem.AddHs(mol)
+        mol = Chem.AddHs(mol, addCoords=True)
         return mol
 
-def conformer_selection(mol: Mol, num_confs: int, filename_1: str)-> Union[None, int]:
+def contains_metal(mol: Mol) -> bool:
+    '''
+    Detects whether a molecule contains a metal atom,
+    indicating possible coordination chemistry.
+    
+    Args:
+        mol (Mol): RDKit molecule object
+        
+    Returns:
+        bool: True in case of detection of a metal
+        bool: False in case of non-detection of a metal
+    '''
+
+    metal_atomic_numbers: set = {
+    # s-block metals
+    3, 4, 11, 12, 19, 20, 37, 38, 55, 56,
+
+    # d-block (transition metals)
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+    72, 73, 74, 75, 76, 77, 78, 79, 80,
+    104, 105, 106, 107, 108, 109, 110, 111, 112,
+
+    # p-block metals (post-transition)
+    13, 31, 49, 50, 81, 82, 83,
+
+    # lanthanides
+    57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+
+    # actinides
+    89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103}
+
+    for atom in mol.GetAtoms():
+        if atom.GetAtomicNum() in metal_atomic_numbers:
+            return True
+    return False
+
+def conformer_selection(mol: Mol, num_confs: Union[int, None], filename_1: str)-> Union[None, int]:
     '''
     Gives the index of the most stable conformer found for the molecule.
     Paralelly builds the SDF file for the said most stable conformer
@@ -54,31 +75,56 @@ def conformer_selection(mol: Mol, num_confs: int, filename_1: str)-> Union[None,
         None: Indicate the failure of the generation
         int: Index of the most stable conformer found
     '''
-    conf_ids: list[int] = AllChem.EmbedMultipleConfs(mol, numConfs=num_confs)
-    if not conf_ids:
-        return (None)
-    energies: dict[int,float] = {}
-    for conf_id in conf_ids:
-        ff = AllChem.UFFGetMoleculeForceField(mol, confId=conf_id)
-        ff.Minimize()
-        energies[conf_id] = ff.CalcEnergy()
-    most_stable_conformer:int = min(energies, key=energies.get)
-    mol_block: str=Chem.MolToMolBlock(mol,confId=most_stable_conformer)
-    with open(filename_1, "w") as file:
-        file.write(mol_block)
-    return most_stable_conformer
+    if mol.GetNumAtoms()<3:
+        conf_id: int=AllChem.EmbedMolecule(mol)
+        if conf_id == -1:
+            return None
+        else:
+            sdf_block: str=Chem.MolToMolBlock(mol,confId=conf_id)
+            with open(filename_1, "w") as file:
+                file.write(sdf_block)
+            return conf_id
+    else:
+        if num_confs == None:
+            num_confs=10000
+        params=AllChem.ETKDGv3()
+        params.pruneRmsThresh = 0.5               
+        conf_ids = AllChem.EmbedMultipleConfs(mol, numConfs=num_confs, params=params)
+        if not conf_ids:
+            return (None)
+        else:
+            energies: dict[int,float] = {}
+            props = AllChem.MMFFGetMoleculeProperties(mol)
+            for conf_id in conf_ids:
+                if props != None:
+                    ff = AllChem.MMFFGetMoleculeForceField(mol,props, confId=conf_id)
+                    if ff == None:
+                        ff = AllChem.UFFGetMoleculeForceField(mol, confId=conf_id)
+                else:
+                    ff = AllChem.UFFGetMoleculeForceField(mol, confId=conf_id)
+
+                if ff == None:
+                    continue
+                ff.Minimize()
+                energies[conf_id] = ff.CalcEnergy()
+            if energies == {}:
+                return None
+            most_stable_conformer:int = min(energies, key=energies.get)
+            with open(filename_1, "w") as file:
+                file.write(Chem.MolToMolBlock(mol,confId=most_stable_conformer))
+            return most_stable_conformer
   
 
-def overall_conversion(molecule_name: str, num_confs: int=10000, filename_1: Union[str, None]=None, filename_2: Union[str, None]=None )-> str:
+def overall_conversion(smiles: str, num_confs: Union[None, int], filename_1: Union[str, None]=None, filename_2: Union[str, None]=None )-> str:
     '''
     Gives the xyz block (str) of the molecules introduced through its smiles. Generate the xyz file
     parallelly, as well as the SDF file.
     Gives a message (str) in case of invalid smiles or molecule.
     Gives a message (str) in case of failure in the conformer generation.
     Gives a message (str) in case of failure of the identification of the molecule.
-
+        
     Args:
-        molecule_name (str): Name of the studied molecule
+        smiles (str): Name of the studied molecule
         num_confs (int): Number of conformers to generate.
         
         
@@ -88,31 +134,35 @@ def overall_conversion(molecule_name: str, num_confs: int=10000, filename_1: Uni
         str: "Unidentified molecule" to indicate that the name of the studied molecule
         is not found in the pubchem library.
         of the smiles, or the chemical nonsenseless of the studied molecule.
-        str: xyz block of the studied molecules in case of absence of problems.
-        Its content must be the same as the xyz file created.
+        str: "Files succesfully generated" to indicate the succesful generation of SDF and
+        xyz files.
+        str: "3D structure not found" to indicate the impossibility of finfing the appropriate
+        tridimentional structure of the studied coordination compound.
+        str: "Unsupported type of compound" to indicate the impossibility of the script to deal with
+        coordination complexes
         
     '''
-    smiles: Union[str, None]=smiles_obtention(molecule_name)
-    if smiles!= None:
-        mol: Union[Mol,str]=mol_from_SMILES(smiles)
-        if mol !=None:
-            if filename_1 != None:
-                most_stable_conformer:Union[int, str]=conformer_selection(mol, num_confs, filename_1)
-            else:
-                most_stable_conformer:Union[int, str]=conformer_selection(mol, num_confs, f"{molecule_name}.SDF")
-            if most_stable_conformer != None:
-                xyz:str = Chem.MolToXYZBlock(mol, confId=most_stable_conformer)
-                if filename_2 != None:
-                    with open(filename_2, "w") as file:
-                        file.write(xyz)
-                else:
-                    with open(f"{molecule_name}.xyz", "w") as file:
-                        file.write(xyz)
-                return xyz   
-            else:
-                return "No conformer generated"
-        else:
-            return "Invalid smiles or impossible molecule"
+    mol: Union[Mol, None]=mol_from_SMILES(smiles)
+    if mol == None:
+        return "Invalid smiles or impossible molecule"
     else:
-        return "Unidentified molecule"
+        test_metal: bool=contains_metal(mol)
+        if test_metal == False:
+            if filename_1 == None:
+                most_stable_conformer: Union[int, None]=conformer_selection(mol, num_confs, f"{molecule_name}.SDF")
+            else:
+                most_stable_conformer: Union[int, None]=conformer_selection(mol, num_confs, filename_1)            
+            if most_stable_conformer == None:
+                return "No conformer generated"
+            else:
+                xyz_block=Chem.MolToXYZBlock(mol, confId=most_stable_conformer)
+                if filename_2 == None:
+                    with open(f"{molecule_name}.xyz", "w") as f:
+                        f.write(xyz_block)
+                else:
+                    with open(filename_2, "w") as f:
+                        f.write(xyz_block)
+                return "Files succesfully generated"
+        else:
+            return "Unsupported type of compound"
 
