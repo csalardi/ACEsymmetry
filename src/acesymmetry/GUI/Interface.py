fa0d55231c23ef1e0ec150a2fbf6f2a87e94656e
escherz <elodie.scherz@epfl.ch>
2026-05-24 16:38:54 +0200

    add flowchart

--- src/acesymmetry/GUI/Interface.py
+++ src/acesymmetry/GUI/Interface.py
@@ -6,18 +6,20 @@ from acesymmetry import Visual_Display, Format_Conversion
 
 
 st.set_page_config(
-    page_title="Molecular symmetry by ACE",
+    page_title="ACEsymmetry app",
     page_icon="",
     layout="wide",
     initial_sidebar_state="collapsed",)
 st.logo("assets/epfl-logo.svg", size="large" , link="https://www.epfl.ch/en/", icon_image=None,)
     
-st.title(" ACEsymmetry app")
-st.subheader("Welcome in our ACEsymmetry app ")
+st.title("Welcome in our ACEsymmetry app")
 st.write("This app is an interactive application developed at EPFL for the Practical programming in Chemistry. etc. ")
 st.text("This app allows users to: - analyse molecular symmetry")
 col1, col2, col3 = st.columns([1,5,2])
 col3.markdown("-- ACESymmetry")
-if col3.button("Next →"):
+if col3.button("Group Point"):
     st.session_state.current = 0
     st.switch_page("pages/page1.py")
+elif col3.button("Learning mode"):
+    st.session_state.current = 0
+    st.switch_page("pages/learning_mode.py")
