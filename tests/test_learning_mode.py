4e1d4d0a4b35f6ba0fc1cd21674f59cfdb806003
AVayron <alexis.vayrondelamoureyre@epfl.ch>
2026-05-24 16:32:30 +0200

    development

--- tests/test_learning_mode.py
+++ tests/test_learning_mode.py
@@ -2,3 +2,6 @@ from acesymmetry.GUI.pages import learning_mode as lm
 print(lm.check_linearity("Dinfh"))
 print(lm.normalize_label("C2''"))
 print(lm.parse_symmetry_element("3 sigma(xy)"))
+print(lm.check_inversion_centre({"E","C4","4 C2","Sigma_h","2 Sigma_v","2 Sigma_d","i"}))
+print(lm.check_main_axis_multiplicity("Oh"))
+print(lm.check_rotation_axis([]))
